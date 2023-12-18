5a23119888b0794bed4f4c764779c3fd41de4893
drenmorina12 <drenmorina321@gmail.com>
2023-12-18 17:48:33 +0100

    Add function to change square color
    add function to check if mouse is up or down.

--- app.js
+++ app.js
@@ -4,15 +4,34 @@ const gridRows = 16;
 const gridHeight = 600;
 const squares = gridRows * gridRows;
 const squareHeight = gridHeight / gridRows;
+let click = false;
+
+function getMouseStatus() {
+  grid.addEventListener("mousedown", () => {
+    click = true;
+  });
+  grid.addEventListener("mouseup", () => {
+    click = false;
+  });
+}
 
 function createGrid() {
+  getMouseStatus();
   for (let i = 0; i < squares; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
+    square.setAttribute("data-id", i);
     square.style.height = `${squareHeight}px`;
     square.style.width = `${squareHeight}px`;
     grid.appendChild(square);
+    square.addEventListener("mouseover", changeSquareColor);
   }
 }
 
 createGrid();
+
+function changeSquareColor() {
+  if (click) {
+    this.style.backgroundColor = "black";
+  }
+}
